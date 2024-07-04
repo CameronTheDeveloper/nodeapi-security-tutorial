e3046e060d25f88382fb03a224b2312bbaa1a98e
CameronTheDeveloper <Cameronposey@outlook.com>
2024-07-03 20:22:53 -0700

    Implement api login POST operation

--- app.js
+++ app.js
@@ -15,4 +15,19 @@ app.post('/api/posts', (req, res) => {
     });
 });
 
+app.post('/api/login', (req, res) => {
+    //Mock user
+    const user = {
+        id: 1,
+        username: 'john',
+        email: 'john@gmail.com'
+    };
+
+    jwt.sign({ user: user }, 'secretkey', (err, token) => {
+        res.json({
+            token: token
+        });
+    });
+});
+
 app.listen(4000, () => console.log('Server started on port 4000'));
