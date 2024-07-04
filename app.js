3ca8d227f1e5436263558ee103d21c1e0b5ba58a
CameronTheDeveloper <Cameronposey@outlook.com>
2024-07-03 21:48:18 -0700

    Add verifyToken middleware

--- app.js
+++ app.js
@@ -3,13 +3,38 @@ const jwt = require('jsonwebtoken');
 
 const app = express();
 
+// Format Of Token:
+// Authorization: Bearer <access_token>
+
+// Verify Token
+
+function verifyToken(req, res, next) {
+    // Get auth header value
+    const bearerHeader = req.headers['authorization'];
+    // Check if bearer is undefined
+    if (typeof bearerHeader !== 'undefined') {
+        // Split at the space
+        const bearer = bearerHeader.split(' ');
+        // Get token from array
+        const bearerToken = bearer[1];
+        // Set token
+        req.token = bearerToken;
+        // Next middleware
+        next();
+    } else {
+        // Forbidden
+        res.sendStatus(403);
+    }
+
+}
+
 app.get('/api', (req, res) => {
     res.json({
         message: 'Welcome to the API'
     });
 });
 
-app.post('/api/posts', (req, res) => {
+app.post('/api/posts', verifyToken, (req, res) => {
     res.json({
         message: 'Post created...'
     });
